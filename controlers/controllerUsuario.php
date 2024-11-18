f61b8894805dd5372586c8bdc6634d1d32d4cf3a
higuinha123 <higafontourakaua@mail.com>
2024-11-18 19:35:08 -0400

    terminando controller

--- controlers/controllerUsuario.php
+++ controlers/controllerUsuario.php
@@ -1,10 +1,29 @@
 <?php
 
 require "../models/usuario.php";
-require "../config/database.php"
+require "../config/database.php";
 
-class controller {
+class usuariocontroller {
     protected $tabela= "usuario";
-   
+    public $conexao;
+
+    public function conectarBd(){
+        $this->conexao = (new bd())->conectar();
+        return $this->conexao;
+    }
+
+    public function cadastrar($nome, $data_nasc, $email, $senha, $endereco) {
+        $usuario = new Usuario($this->conectarBd());
+        $query = $usuario->cadastrar();
+
+        $stmt = $this->conexao->prepare($query);
+        $senhahash = password_hash($senha, PASSWORD_BCRYPT);
+        $stmt->bind_param("sdasd", $nome, $data_nasc, $email, $senhahash, $endereco);
+        $stmt->execute();
+
+        $this->conexao->close();
+    }
 }
+   
+
 ?>
--- view/login.php
+++ view/login.php
@@ -1,14 +1,43 @@
+<?php
+require "../models/usuario.php";
+require "../config/database.php";
+require "../controlers/controllerUsuario.php"
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
-    
+    <link rel="stylesheet" href="./views/style.css">
 </head>
 <body>
  
-    
+    <header>
+        <nav class="container">
+           
+            <div class="logo-box">
+                <a href="./index.php">
+                    <img src="./imgs/icone.png" alt="Logo" class="logo">
+                </a>
+            </div>
+         
+            <div class="parent-link">
+                <a href="#" class="social-links">Sobre</a>
+                <a href="#" class="social-links">Projetos</a>
+                <a href="#" class="social-links">Experiência</a>
+                <a href="#" class="social-links">Contato</a>
+            </div>
+           
+            <div class="social-icons">
+                <a href="#" class="social-icon"><img src="./imgs/ig_icone.png" alt="Instagram"></a>
+                <a href="#" class="social-icon"><img src="./imgs/Twitter-Logo.png" alt="Twitter"></a>
+                <a href="#" class="social-icon"><img src="./imgs/github.png" alt="GitHub"></a>
+            </div>
+        </nav>
+    </header>
+ 
  
    
     <div class="form">
@@ -23,23 +52,60 @@
                
                 <label for="senha">Senha</label>
                 <input type="password" id="senha" name="Senha" required><br>
-
-                <label for="email">Email</label>
-                <input type="text" id="email" name="Email" required><br>
-
-                <label for="endereço">endereço</label>
-                <input type="endereço" id="endereço" name="endereço" required><br>
-
-                <label for= "data">Data De Nascimento</label>
-                <input id="date" type="date" />
-
  
                
                 <a href="home.php" class="custom-link">Login</a>
             </div>
  
          
-           
+            <a href="index.php" class="voltar">Voltar</a>
+ 
+         
+            <div class="button-form">
+                <p>Você não tem conta? <a href="cadastro.php" class="Cadastrar-se">Cadastrar-se</a></p>
+               
+            </div>
+        </form>
+    </div>    
+    <footer>
+    <div class="footer-container">
+        <div class="footer-links">
+            <h3>Links Rápidos</h3>
+            <ul>
+                <li><a href="#">Sobre nós</a></li>
+                <li><a href="#">Projetos</a></li>
+                <li><a href="#">Experiência</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </div>
+       
+        <div class="footer-social">
+            <h3>Redes Sociais</h3>
+            <ul>
+                <li><a href="https://www.instagram.com" target="_blank">
+                    <img src="./imgs/ig_icone.png" alt="Instagram" class="social-icon">
+                </a></li>
+                <li><a href="https://twitter.com" target="_blank">
+                    <img src="./imgs/Twitter-Logo.png" alt="Twitter" class="social-icon">
+                </a></li>
+                <li><a href="https://github.com" target="_blank">
+                    <img src="./imgs/github.png" alt="GitHub" class="social-icon">
+                </a></li>
+            </ul>
+        </div>
+       
+        <div class="footer-contact">
+            <h3>Contato</h3>
+            <p>Email: <a href="mailto:contato@dominio.com">Higuinha@gmail.com</a></p>
+            <p>Telefone: (67) 56311-8969</p>
+        </div>
+    </div>
+   
+    <div class="footer-bottom">
+        <p>&copy; 2024 Biblioteca141. Todos os direitos reservados.</p>
+    </div>
+</footer>
        
 </body>
 </html>
+ 
